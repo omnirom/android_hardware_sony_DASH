47da4df9d352dcd0db118e2721cf0f890583a0a4
CamberCreak <cambercreak@gmail.com>
2014-09-21 09:59:28 +0100

    Only build akm8963 dummy when required
    Change-Id: I96a970e9e2c7bc072fbf04005533271ea22d1427

--- libs/akm8963/Android.mk
+++ libs/akm8963/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(SOMC_CFG_SENSORS_COMPASS_AK8963),)
+
 LOCAL_MODULE := libsensors_akm8963
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES += ak8963_dummy.c
 include $(BUILD_SHARED_LIBRARY)
+
+endif
